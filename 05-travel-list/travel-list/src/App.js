58cb2330ed076e2f548e1419ed851b880f812455
Eugeniu Savca <6307017+e-savca@users.noreply.github.com>
2024-12-18 21:45:07 +0200

    Added header, logo, form and stats

--- 05-travel-list/travel-list/src/App.js
+++ 05-travel-list/travel-list/src/App.js
@@ -1,14 +1,39 @@
-function App() {
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        Learn React
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>🏝️ Far Away ✈️</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <ul className="list">{initialItems.map}</ul>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>🧳 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
